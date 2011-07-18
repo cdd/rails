3386a76384483f5478452be661e1c84bd2398411
David Blondeau <david@collaborativedrug.com>
2011-07-18 12:58:58 -0700

    CDD - performance hack to get Rails working without ActiveSupport::JSON encoding

--- activesupport/lib/active_support/json.rb
+++ activesupport/lib/active_support/json.rb
@@ -1,2 +1,24 @@
 require 'active_support/json/decoding'
-require 'active_support/json/encoding'
+
+# CDD performance hack to try to get Rails working without ActiveSupport::JSON encoding
+# require 'active_support/json/encoding'
+require 'json/ext'
+
+# if we re-enable the active_support json encoding, uncomment the following defaults
+
+# Use ISO 8601 format for JSON serialized times and dates
+# ActiveSupport.use_standard_json_time_format = true
+
+# Don't escape HTML entities in JSON, leave that for the #json_escape helper
+# if you're including raw json in an HTML page.
+# ActiveSupport.escape_html_entities_in_json = false
+
+module ActiveSupport
+  module JSON
+    class << self
+      def encode(thingy)
+        thingy.to_json
+      end
+    end
+  end
+end  
